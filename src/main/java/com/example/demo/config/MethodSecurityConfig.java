721c253a79a74b4ef15f74f117366d04b6aa35ab
Jaiswal <2029798@cognizant.com>
2021-08-21 16:16:16 +0530

    New onSuccessHandler in security config file

--- src/main/java/com/example/demo/config/MethodSecurityConfig.java
+++ src/main/java/com/example/demo/config/MethodSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableGlobalMethodSecurity(
@@ -11,4 +15,17 @@ import org.springframework.security.config.annotation.method.configuration.Globa
   jsr250Enabled = true)
 public class MethodSecurityConfig 
   extends GlobalMethodSecurityConfiguration {
+	
+	/*@Autowired
+	@Qualifier("oauth2authSuucessHandler")
+	private AuthenticationSuccessHandler oauth2authSuucessHandler;*/
+	
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/","/myapp/login").permitAll()
+		.anyRequest().authenticated().and().csrf().disable()
+		//.formLogin().loginPage("/login").defaultSuccessUrl("/success")
+		.oauth2Login().successHandler(oauth2authSuucessHandler);
+		
+	}*/
 }
