f823184ff2c104ad227f45afcdf9066754af32a8
Jaiswal <2029798@cognizant.com>
2021-08-21 11:51:17 +0530

    New spring-security-oauth code

---
+++ src/main/java/com/example/demo/SpringSecurityOauth2Github1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2Github1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOauth2Github1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/hello")
+	//@PreAuthorize("#oauth2.hasScope('read')")
+    public String helloSecured() {
+        return "Hello - Secured";
+    }
+	
+	
+	@GetMapping("/token")
+    public Map<String, Jwt> getToken(@AuthenticationPrincipal Jwt jwt) {
+        return Collections.singletonMap("Principal", jwt);
+    }
+	
+}
